006270250a3bd01eaa646016779c8adba7e47164
Tristan Thompson <tristankoffee@gmail.com>
2018-08-29 22:19:39 +0100

    Making music with button presses

--- src/a-or-b.py
+++ src/a-or-b.py
@@ -1,6 +1,6 @@
 from microbit import *
 
-display.scroll("Press A or B")
+# display.scroll("Press A or B")
 
 while True:
     if button_a.was_pressed():
--- src/basic-music.py
+++ src/basic-music.py
@@ -1,13 +1,29 @@
 from microbit import *
 import music 
 
-from microbit import *
-import music 
-
-tune = ["C4:4", "D4:4", "E4:4", "C4:4", "C4:4", "D4:4", "E4:4", "C4:4",
-"E4:4", "F4:4", "G4:8", "E4:4", "F4:4", "G4:8"]
+tune = ["C4:4", "D4:4", "E4:4", "C4:4", "C4:4", "D4:4", "E4:4", 
+        "C4:4", "E4:4", "F4:4", "G4:8", "E4:4", "F4:4", "G4:8"]
+        
+tt = Image("99900:"
+           "09000:"
+           "00000:"
+           "00999:"
+           "00090")
 
 # NB - this needs croc clips connected to headphones to work
 # "0" to the bottom of the jack
 # "GND" to the top of the jack
-music.play(tune)
+while True:
+    while True:
+        # while True:
+        display.show("A")
+        sleep(350)
+        display.show("B")
+        sleep(350)
+        if button_a.was_pressed():
+            break
+        elif button_b.was_pressed():
+            break
+    display.show(Image.MUSIC_QUAVERS)
+    # display.show(tt)
+    music.play(tune)
